589905e3c7b15d8e19ae63074898bcf25c659371
GMDorneles <gabrielly.maldonadoo@gmail.com>
2021-07-18 10:58:38 -0400

    reparos

--- Exercícios Lógicos/_1.9.js
+++ Exercícios Lógicos/_1.9.js
@@ -1,20 +1,19 @@
 function TransformarEmMatriz(String){
     let elemetos;
-    let k=0;
    matriz= new Array( elemetos);
    let i=0;
+   String=String.replace(/[^0-9]/g,'');
+   console.log(String);
    while(i<(String.length)){
-        let arr=new Array(3);
-        for(let j=0;j<=2||i<(String.length);j++){
-            arr[j]=String[i];
+        new Array();
+        for(let j=0;j<=1;j++){
+            console.log(String[i]);
+            Array[j]=String[i]; 
             i++;
         }
-        i++;
-        matriz[k]=arr;
-   } 
-   matriz.forEach(item =>{
-        console.log(item);
+        matriz.push(Array((String[i-2]),(String[i-1])));
 
-    });
+   } 
+      console.log(matriz);
 }
 TransformarEmMatriz("1,2,3,4,5,6");
